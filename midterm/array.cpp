10ae9763d0fbb07c368ea4643e96cc7670517448
Dylan Donovan <ohmygodtheskyisfalling@gmail.com>
2018-10-24 21:48:26 -0700

    finished first midterm function

--- midterm/array.cpp
+++ midterm/array.cpp
@@ -4,12 +4,22 @@
 
 //put the implementations of your assigned functions here
 
-
+//compute and return the number of even integers in list
 int numOfEven(int list[], int size)
 {
-    return 0;
+    int index;
+    int count = 0;
+    for(index=0;index < size; index++)
+    {
+        if(list[index] % 2 == 0)
+        {
+            cout << list[index] << " ";
+            count++;
+        }
+    }
+    return count;
 }
-
+//insert newInt into the list at index “position” and update the size of the list
 void insert(int list[], int& size, int newInt, int position)
 {
     return;
--- midterm/main.cpp
+++ midterm/main.cpp
@@ -4,14 +4,19 @@ using namespace std;
 const int CAP = 100;
 int main()
 {
+    int count;
+    int newInt = 19;
+    int position = 3;
 	int list[CAP];
 	int size = 10;
     build(list, size);
     display(list, size);
-
     //PLEASE PUT YOUR CODE HERE to call the function assigned
-
+    count = numOfEven(list, size);
+    cout << count;
+    insert(list, size, newInt, position)
     display(list, size);
+    
     
     return 0;
 }
