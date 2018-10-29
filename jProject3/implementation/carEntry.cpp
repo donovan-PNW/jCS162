b39009e0261760ac5a616822bb50882040e31db2
Dylan Donovan <ohmygodtheskyisfalling@gmail.com>
2018-10-29 14:00:37 -0700

    mon 1400 need read/write for enums

--- jProject3/implementation/carEntry.cpp
+++ jProject3/implementation/carEntry.cpp
@@ -112,13 +112,68 @@ whereFrom carEntry::getOrigin() const
 {
     return origin;
 }
-////print video
-//void printCarEntry();
-//void printCarEntry(ofstream &);		
+void carEntry::printCarEntry()
+{
+    char ioName[MAX_CHAR];
+    printCarEntry
+}
+void carEntry::printCarEntry(ofstream &outFile)
+{
+    outfile <<  
+}
+
+//LOOK AT THESE they'll convert enums
+//Genre readGenre(char);
+//void printGenre(Genre, char []);
 
 
-
-
+//This function assigns a Genre based on the letter and returns the Genre
+//Genre readGenre(char letter)
+//{
+//    Genre tempGenre;
+//    switch(tolower(letter))
+//    {
+//        case 'a':
+//            tempGenre = static_cast<Genre>(0);
+//            break;
+//        case 'f':
+//            tempGenre = static_cast<Genre>(1);
+//            break;
+//        case 'c':
+//            tempGenre = static_cast<Genre>(2);
+//            break;
+//        case 'h':
+//            tempGenre = static_cast<Genre>(3);
+//            break;
+//        default:
+//            tempGenre = static_cast<Genre>(4);
+//    }
+//    return tempGenre;
+//}
+//
+////This function populates the genreDesc based on the Genre
+//void printGenre(Genre tempGenre, char genreDesc[])
+//{
+//    switch(tempGenre)
+//    {
+//        case 0:
+//            strcpy(genreDesc, "ACTION");
+//            break;
+//        case 1:
+//            strcpy(genreDesc, "FAMILY");
+//            break;
+//        case 2:
+//            strcpy(genreDesc, "COMEDY");
+//            break;
+//        case 3:
+//            strcpy(genreDesc, "HORROR");
+//            break;
+//        case 4:
+//            strcpy(genreDesc, "ILLEGAL");
+//            break;
+//    }
+//    return;
+//}
 
 
 
